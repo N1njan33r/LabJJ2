e85c5ceedaaf152758cabb6be9dec75a0c34caf2
Alexander Inman <fujiwara06@gmail.com>
2019-01-13 19:57:32 -0500

    First thing

---
+++ LabJJ2/Controllers/ProductController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace LabJJ2.Controllers
+{
+    public class ProductController : Controller
+    {
+        // GET: Product
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Edit(ProductController model)
+        {
+            if (ModelState.IsValid)
+            {
+                return RedirectToRoute(new { controller = "Home", action = "Index" });
+            }
+
+            return View("Index", model);
+        }
+    }
+}
---
+++ LabJJ2/Models/Products.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace LabJJ2.Models
+{
+    public class Products
+    {
+        [Required]
+        [MaxLength(20)]
+        public string Name { get; set; }
+
+        [Required]
+        [MaxLength(50)]
+        public string Description { get; set; }
+
+        [Required]
+        // Test for double or for currency
+        public double Price { get; set; }
+    }
+}
